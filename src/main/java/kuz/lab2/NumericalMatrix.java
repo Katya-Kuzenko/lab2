60c30fe4ab74646d2c0bcf4d5460e24a42e15c04
Katya-Kuzenko <keyt.keuz@gmail.com>
2023-11-29 21:34:59 +0100

    add tests; add validation

--- src/main/java/kuz/lab2/NumericalMatrix.java
+++ src/main/java/kuz/lab2/NumericalMatrix.java
@@ -7,6 +7,9 @@ public class NumericalMatrix implements Matrix{
         matrix = new double[0][0];
     }
     public NumericalMatrix(int m, int n) {
+        if (m < 0 || n < 0) {
+            throw new IllegalArgumentException("Invalid row on column size");
+        }
         matrix = new double[m][n];
     }
     public NumericalMatrix(NumericalMatrix numericalMatrix) {
@@ -19,26 +22,43 @@ public class NumericalMatrix implements Matrix{
     }
 
     public void setElement(int row, int column, double el){
+        validateRow(row);
+        validateColumn(column);
         matrix[row][column] = el;
+    }
+
+    public void setMatrix(double[][] matrix) {
+        if (matrix.length != getRowNumbers() || matrix[0].length != this.getColumnNumbers()) {
+            throw new IllegalArgumentException("Matrix with wrong size");
+        }
+        for (int i = 0; i < getRowNumbers(); i++){
+            for (int j = 0; j < getColumnNumbers(); j++){
+                this.matrix[i][j] = matrix[i][j];
+            }
+        }
     }
 
     @Override
     public double getElement(int row, int column) {
+        validateRow(row);
+        validateColumn(column);
         return matrix[row][column];
     }
 
     @Override
     public double[] getRow(int index) {
+        validateRow(index);
         return matrix[index];
     }
 
     @Override
     public double[] getColumn(int index) {
+        validateColumn(index);
         double[] column = new double[getRowNumbers()];
-        for (int i = 0; i < getRowNumbers(); i++){
+        for (int i = 0; i < getRowNumbers(); i++) {
             column[i] = matrix[i][index];
         }
-        return new double[0];
+        return column;
     }
 
     @Override
@@ -52,5 +72,17 @@ public class NumericalMatrix implements Matrix{
             return 0;
         }
         return matrix[0].length;
+    }
+
+    private void validateColumn(int column) {
+        if (column < 0 || column >= getColumnNumbers()) {
+            throw new IllegalArgumentException("Invalid column");
+        }
+    }
+
+    private void validateRow(int row) {
+        if (row < 0 || row >= getRowNumbers()) {
+            throw new IllegalArgumentException("Invalid row");
+        }
     }
 }
---
+++ src/test/java/NumericalMatrixTest.java
@@ -0,0 +1,67 @@
+import kuz.lab2.NumericalMatrix;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NumericalMatrixTest {
+    private NumericalMatrix numericalMatrix;
+
+    @BeforeEach
+    public void init() {
+        /*
+            1 2 3
+            3 2 1
+            7 4 8
+         */
+        numericalMatrix = new NumericalMatrix(3, 3);
+        numericalMatrix.setMatrix(new double[][]{{1, 2, 3}, {3, 2, 1}, {7, 4, 8}});
+    }
+
+    @Test
+    public void testSetGetMatrixElement() {
+        numericalMatrix.setElement(0, 0, 99);
+        assertEquals(99, numericalMatrix.getElement(0, 0));
+    }
+
+    @Test
+    public void tesGetMatrixColumn() {
+        double[] column = numericalMatrix.getColumn(2);
+        assertArrayEquals(new double[]{3, 1, 8}, column);
+    }
+
+    @Test
+    public void tesGetMatrixRow() {
+        double[] row = numericalMatrix.getRow(0);
+        assertArrayEquals(new double[]{1, 2, 3}, row);
+    }
+
+    @Test
+    public void testGetRowNumbers() {
+        assertEquals(3, numericalMatrix.getRowNumbers());
+    }
+
+    @Test
+    public void testGetColumnNumbers() {
+        assertEquals(3, numericalMatrix.getColumnNumbers());
+    }
+
+    @Test
+    public void testDimension() {
+        assertEquals(9, numericalMatrix.dimension());
+    }
+
+
+    @Test
+    public void testThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> numericalMatrix.getColumn(-1));
+        assertThrows(IllegalArgumentException.class, () -> numericalMatrix.getColumn(3));
+        assertThrows(IllegalArgumentException.class, () -> numericalMatrix.getRow(-1));
+        assertThrows(IllegalArgumentException.class, () -> numericalMatrix.getRow(3));
+        assertThrows(IllegalArgumentException.class, () -> numericalMatrix.getElement(-1, 1));
+        assertThrows(IllegalArgumentException.class, () -> numericalMatrix.getElement(1, -1));
+        assertThrows(IllegalArgumentException.class, () -> numericalMatrix.setElement(3, 1, 99));
+        assertThrows(IllegalArgumentException.class, () -> numericalMatrix.setElement(1, 3, 99));
+    }
+
+
+}
