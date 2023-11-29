315b29a73ec5c25d2f8606cc0947ff7f48db7650
Katya-Kuzenko <keyt.keuz@gmail.com>
2023-11-29 18:34:22 +0100

    add constructors

--- src/main/java/kuz/lab2/NumericalMatrix.java
+++ src/main/java/kuz/lab2/NumericalMatrix.java
@@ -3,5 +3,13 @@ package kuz.lab2;
 public class NumericalMatrix implements Matrix{
     private final double[][] matrix;
 
-
+    public NumericalMatrix() {
+        matrix = new double[0][0];
+    }
+    public NumericalMatrix(int m, int n) {
+        matrix = new double[m][n];
+    }
+    public NumericalMatrix(NumericalMatrix numericalMatrix) {
+        matrix = new double[numericalMatrix.matrix.length][numericalMatrix.matrix[0].length];
+    }
 }
