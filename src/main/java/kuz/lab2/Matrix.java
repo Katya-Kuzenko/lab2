407a2649f4aafe2b14920e140dec06ab3676489c
Katya-Kuzenko <keyt.keuz@gmail.com>
2023-11-29 20:05:59 +0100

    add methods set, get matrix element; get matrix dimension; get matrix row and column

--- src/main/java/kuz/lab2/Matrix.java
+++ src/main/java/kuz/lab2/Matrix.java
@@ -2,4 +2,13 @@ package kuz.lab2;
 
 public interface Matrix {
 
+    double getElement(int row, int column);
+    double[] getRow(int index);
+    double[] getColumn(int index);
+    int getRowNumbers();
+    int getColumnNumbers();
+    default int dimension(){
+        return getColumnNumbers() * getRowNumbers();
+    }
+
 }
--- src/main/java/kuz/lab2/NumericalMatrix.java
+++ src/main/java/kuz/lab2/NumericalMatrix.java
@@ -10,10 +10,47 @@ public class NumericalMatrix implements Matrix{
         matrix = new double[m][n];
     }
     public NumericalMatrix(NumericalMatrix numericalMatrix) {
-        matrix = new double[numericalMatrix.matrix.length][numericalMatrix.matrix[0].length];
+        matrix = new double[numericalMatrix.getRowNumbers()][numericalMatrix.getColumnNumbers()];
+        for (int i = 0; i < getRowNumbers(); i++){
+            for (int j = 0; j < getColumnNumbers(); j++){
+                matrix[i][j] = numericalMatrix.matrix[i][j];
+            }
+        }
     }
 
     public void setElement(int row, int column, double el){
         matrix[row][column] = el;
+    }
+
+    @Override
+    public double getElement(int row, int column) {
+        return matrix[row][column];
+    }
+
+    @Override
+    public double[] getRow(int index) {
+        return matrix[index];
+    }
+
+    @Override
+    public double[] getColumn(int index) {
+        double[] column = new double[getRowNumbers()];
+        for (int i = 0; i < getRowNumbers(); i++){
+            column[i] = matrix[i][index];
+        }
+        return new double[0];
+    }
+
+    @Override
+    public int getRowNumbers() {
+        return matrix.length;
+    }
+
+    @Override
+    public int getColumnNumbers() {
+        if(matrix.length == 0){
+            return 0;
+        }
+        return matrix[0].length;
     }
 }
